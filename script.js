55d9e995d16ffefd45a7a7265663efe3e80912d0
Rafael Ferreira <rafael.bemestar@gmail.com>
2024-03-20 15:56:55 -0300

    Update js

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
-const textArea = document.querySelector(".text-area");
-const mensagem = document.querySelector(".mensagem");
-const btnCopiar = document.querySelector('.btn-copiar');
+let permitido_copiar = true;
 
+const aviso = document.querySelector(".conteudo-esquerda-regras p");
+const input = document.querySelector(".texto");
+const botao_criptografar = document.querySelector(".criptografar");
+const botao_descriptografar = document.querySelector(".descriptografar");
 
-// As "chaves" de criptografia que utilizaremos são:
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "a" é convertida para "ai"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
+const mensagem_titulo = document.querySelector(".conteudo-direita-mensagem h2");
+const mensagem_paragrafo = document.querySelector(".mensagens p");
+const texto_conteudo_direita = document.querySelector(".texto-criptografado");
+const imagens_conteudo_direita = document.querySelector(".conteudo-direita-itens");
+const botao_copiar = document.querySelector(".conteudo-direita-copiar");
 
-function btnEncriptar() {
-    const textoEncriptado = encriptar(textArea.value);
-    mensagem.value = textoEncriptado;
-    textArea.value = "";
-}
+input.addEventListener("input", function () {
+    const texto = input.value;
+    // Voltar a imagem padrão quando não houver texto
+    if (texto == "") {
+        texto_conteudo_direita.style.display = "none";
+        imagens_conteudo_direita.style.display = "flex";
+        botao_copiar.style.display = "none";
+    }
 
-function encriptar(stringEncriptada) {
-    
-    let matrizCodigo = [["e" , "enter"],["i" , "imes"],["a" , "ai"],["o" , "ober"],["u" , "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
+    if (/[^a-z, ]/.test(texto)) {
+        aviso.style.color = "#ec2828";
+        permitido_copiar = false;
+        mensagem_titulo.style.color = "#ec2828"
+        mensagem_titulo.innerHTML = "Mensagem incorreta!"
+        mensagem_paragrafo.innerHTML = "Apenas letras minúsculas e sem acento."
+    } else if (texto == "") {
+        aviso.style.color = "#495057";
+        mensagem_titulo.style.color = "#000000"
+        mensagem_titulo.innerHTML = "Nenhuma mensagem encontrada"
+        mensagem_paragrafo.innerHTML = "Digite um texto que você deseja criptografar ou descriptografar."
+    } else {
+        aviso.style.color = "#495057";
+        permitido_copiar = true;
+        mensagem_titulo.style.color = "#0A3871"
+        mensagem_titulo.innerHTML = "Mensagem encontrada com sucesso!"
+        mensagem_paragrafo.innerHTML = "Pronto para criptografar ou descriptografar."
+    }
+});
 
-    for(let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+botao_criptografar.addEventListener("click", function () {
+    const texto = input.value;
+    let texto_criptografado = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+    if (texto != "") {
+        if (permitido_copiar) {
+            navigator.clipboard.writeText(input.value);
+            botao_criptografar.textContent = "Criptografado!";
+            setTimeout(function () {
+                botao_criptografar.textContent = "Criptografar";
+            }, 2500);
+
+            texto_conteudo_direita.style.display = "block";
+            imagens_conteudo_direita.style.display = "none";
+            botao_copiar.style.display = "block";
+
+            texto_conteudo_direita.innerHTML = texto_criptografado;
+
+        } else {
+            botao_criptografar.textContent = "Texto incorreto!";
+            setTimeout(function () {
+                botao_criptografar.textContent = "Criptografar";
+            }, 2500);
+
+            texto_conteudo_direita.style.display = "none";
+            imagens_conteudo_direita.style.display = "flex";
+            botao_copiar.style.display = "none";
         }
     }
-    return stringEncriptada;    
-}
+});
 
-function btnDesencriptar() {
-    const textoDesencriptado = desencriptar(textArea.value);
-    mensagem.value = textoDesencriptado;
-    textArea.value = "";
-}
+botao_descriptografar.addEventListener("click", function () {
+    const texto = input.value;
+    let texto_desencriptografado = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
-function desencriptar(stringDesencriptada) {
+    if (texto != "") {
+        if (permitido_copiar) {
+            navigator.clipboard.writeText(input.value);
+            botao_descriptografar.textContent = "Descriptografado!";
+            setTimeout(function () {
+                botao_descriptografar.textContent = "Descriptografar";
+            }, 2500);
 
-    let matrizCodigo = [["e" , "enter"],["i" , "imes"],["a" , "ai"],["o" , "ober"],["u" , "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
+            texto_conteudo_direita.style.display = "block";
+            imagens_conteudo_direita.style.display = "none";
+            botao_copiar.style.display = "block";
 
-    for(let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+            texto_conteudo_direita.innerHTML = texto_desencriptografado;
+
+        } else {
+            botao_descriptografar.textContent = "Texto incorreto!";
+            setTimeout(function () {
+                botao_descriptografar.textContent = "Descriptografar";
+            }, 2500);
+
+            texto_conteudo_direita.style.display = "none";
+            imagens_conteudo_direita.style.display = "flex";
+            botao_copiar.style.display = "none";
         }
     }
-    return stringDesencriptada;    
-}
+});
+
+botao_copiar.addEventListener("click", function () {
+    const textoCriptografado = texto_conteudo_direita.textContent;
+    navigator.clipboard.writeText(textoCriptografado)
+
+    botao_copiar.textContent = "Copiado!";
+    setTimeout(function () {
+        botao_copiar.textContent = "Copiar";
+    }, 2500);
+});
